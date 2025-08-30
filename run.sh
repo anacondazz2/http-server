26d2238014adfce59b2dcb2db1f7d65de60ef84b
Patrick Hu <patrickhuuu7@gmail.com>
2025-08-30 01:03:39 -0400

    Port to C++

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-cmake -B build/ -S . -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="-fsanitize=address"
-cmake --build ./build --config Debug
+cmake -B ./build/ -S . 
+cmake --build ./build
 sudo ./build/http-server
